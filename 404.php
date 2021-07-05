451f3196e14cd89936b3bbd321abca960a211310
vnarek577 <81556995+vnarek577@users.noreply.github.com>
2021-07-05 10:25:23 +0200

    error404

--- 404.php
+++ 404.php
@@ -1,1 +1,33 @@
-<!-- init -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Handlee&family=Lobster&family=Poiret+One&display=swap" rel="stylesheet">
+    <link href="./assets/style/style404.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <div class="pageError">
+        <h1><span class="errorcode">404</span></h1>
+        <p class="eror">Oops, la page que vous cherchez semble introuvable. </p>
+        <p class="eror">Veuillez vous revenir à la <a class="but" href="index.php">Page d'accueil</a>.</p>
+        <p class="eror">Good luck.</p>
+    </div>
+
+
+
+
+</body>
+
+</html>
